bd117cdae90127edff722ae47ad131171a3b250d
Filip Loja <filiploja@gmail.com>
2021-02-17 22:57:37 +0100

    Video capturing pt II

--- src/views/ViewVideoRecorder.vue
+++ src/views/ViewVideoRecorder.vue
@@ -1,15 +1,14 @@
 <template>
 	<layout-main title="Video recorder">
+		<video class="video" ref="video" autoplay playsinline muted style="width: 100%"></video>
 		<ion-button @click="recordVideo">Record</ion-button>
-		<div>{{ debug }}</div>
-<!--		<video width="400" controls>-->
-<!--			<source :src="local" type="video/mp4">-->
-<!--			Your browser does not support HTML video.-->
-<!--		</video>-->
-<!--		<video width="400" controls>-->
-<!--			<source :src="full" type="video/mp4">-->
-<!--			Your browser does not support HTML video.-->
-<!--		</video>-->
+		<ion-button @click="stopRecord">Stop</ion-button>
+		<ion-button @click="playVideo('1613598255686.mp4')">Play</ion-button>
+
+		<div id="video-player"></div>
+
+		<video class="video2" ref="player" autoplay controls style="width: 100%"></video>
+
 	</layout-main>
 </template>
 
@@ -18,9 +17,11 @@ import { defineComponent, ref } from 'vue'
 import { IonButton } from '@ionic/vue'
 import { MediaCapture } from '@ionic-native/media-capture'
 
-import {CopyOptions, FilesystemDirectory, Plugins} from '@capacitor/core'
+import { Capacitor, Plugins, FilesystemDirectory } from '@capacitor/core';
+import * as WebVPPlugin from 'capacitor-video-player';
+const { CapacitorVideoPlayer } = Plugins;
+
 const { Filesystem } = Plugins
-import { Capacitor } from '@capacitor/core'
 
 export default defineComponent({
 	name: 'ViewVideoRecorder',
@@ -29,25 +30,113 @@ export default defineComponent({
 		const debug = ref(null)
 		const local = ref(null)
 		const full = ref(null)
+		const video = ref(null)
+		const player = ref(null)
+		const isRecording = ref<boolean>(false)
+
+		let videoPlayer: any = null
+
+
+		if (Capacitor.isNative) {
+			videoPlayer = CapacitorVideoPlayer
+		} else {
+			videoPlayer = WebVPPlugin.CapacitorVideoPlayer
+		}
+
+		let mediaRecorder: any = null
+
+		const convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
+			const reader = new FileReader()
+			reader.onerror = () => reject(null)
+			reader.onload = () => resolve(reader.result)
+			reader.readAsDataURL(blob)
+		})
+
+		const saveVideo = async (blob: Blob) => {
+			const fileName = Date.now() + '.mp4'
+			const base64Data = await convertBlobToBase64(blob) as string;
+			const savedFile = await Filesystem.writeFile({
+				path: fileName,
+				data: base64Data,
+				directory: FilesystemDirectory.Data
+			})
+			console.log(fileName)
+		}
 
 		const recordVideo = async () => {
-			const options = {
-				limit: 1,
-				duration: 10
+			// const options = {
+			// 	limit: 1,
+			// 	duration: 10
+			// }
+			//
+			// MediaCapture.captureVideo(options).then((res: any) => {
+			// 	const capturedFile = res[0]
+			//
+			// 	debug.value = Capacitor.convertFileSrc(capturedFile.fullPath) + '  ---  ' + Capacitor.convertFileSrc(capturedFile.localURL)
+			// })
+
+			isRecording.value = true
+			const stream = await navigator.mediaDevices.getUserMedia({
+				video: { facingMode: 'user' },
+				audio: true
+			})
+
+			video.value.srcObject = stream
+
+			const options = { mimeType: 'video/webm' }
+			// @ts-ignore
+			mediaRecorder = new MediaRecorder(stream, options)
+			let chunks: any[] = []
+
+			mediaRecorder.ondataavailable = (event: any) => {
+				if (event.data && event.data.size > 0) {
+					chunks.push(event.data)
+				}
 			}
 
-			MediaCapture.captureVideo(options).then((res: any) => {
-				const capturedFile = res[0]
+			mediaRecorder.onstop = async () => {
+				const videoBlob = new Blob(chunks, { type: 'video/webm' })
+				saveVideo(videoBlob)
+				chunks = []
+			}
 
-				debug.value = Capacitor.convertFileSrc(capturedFile.fullPath) + '  ---  ' + Capacitor.convertFileSrc(capturedFile.localURL)
+			mediaRecorder.start(100)
+		}
+
+		const stopRecord = () => {
+			mediaRecorder.stop()
+			mediaRecorder = null
+			isRecording.value = false
+			video.value.srcObject = null
+		}
+
+		const loadVideo = async (fileName: string) =>  {
+			const file = await Filesystem.readFile({
+				path: fileName,
+				directory: FilesystemDirectory.Data
 			})
+			return `data:video/mp4;base64,${file.data}`
+		}
+
+		const playVideo = async (fileName: string) => {
+			const videoData = await loadVideo(fileName)
+			player.value.src = videoData
+			// await videoPlayer.initPlayer({
+			// 	mode: 'fullscreen',
+			// 	url: videoData,
+			// 	playerId: 'video-player'
+			// })
 		}
 
 		return {
 			recordVideo,
+			stopRecord,
+			playVideo,
 			debug,
 			local,
-			full
+			full,
+			video,
+			player
 		}
 	}
 })
